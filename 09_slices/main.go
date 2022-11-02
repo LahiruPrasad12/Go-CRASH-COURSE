a9b5a0d96dfd449ac7dcc6748d6ee9aac1ec740d
lahirupr12-cv <lahirub@conceptvines.com>
2022-11-02 10:56:29 +0530

    completed sloces basic part

--- 09_slices/main.go
+++ 09_slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 
@@ -10,7 +13,7 @@ func main() {
 	fmt.Println(fruitList) //-> [apple]
 
 	//add data to slice
-	fruitList = append(fruitList, "Mango", "Banana")
+	fruitList = append(fruitList, "mango", "banana")
 	fmt.Println(fruitList) //-> [apple Mango Banana]
 
 	//get specific part of array
@@ -23,6 +26,22 @@ func main() {
 
 	speicific = append(fruitList[:2])
 	fmt.Println(speicific) //->[apple Mango]
+
+	//sorted string array
+	sort.Strings(fruitList)
+	fmt.Println("Sirted array is:", fruitList) //->[apple banana mango]
+
+	//sorted int array
+	var score = []int{}
+	score = append(score, 2, 5, 3, 10, 4)
+	fmt.Println("array is", score) //-> [2 5 3 10 4]
+	sort.Ints(score)
+	fmt.Println("Sorted array is:", score) // ->[2 3 4 5 10]
+
+	//check whether the array is sorted or not
+	fmt.Println(sort.StringsAreSorted(fruitList)) //-> true
+	fmt.Println(sort.IntsAreSorted(score))        //->true
+
 }
 
 /*
