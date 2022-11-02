71fc1a6552c4f199050a8d088c3f5899eca7d52c
lahirupr12-cv <lahirub@conceptvines.com>
2022-11-02 11:23:10 +0530

    changes

--- 09_slice_part2/main.go
+++ 09_slice_part2/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var courses = []string{"react-js", "javascript", "swift", "python"}
+	fmt.Println(courses)
+
+	//remove value from slice
+	index := 2
+	courses = append(courses[:index], courses[index+1:]...)
+	fmt.Println(courses)
 }
