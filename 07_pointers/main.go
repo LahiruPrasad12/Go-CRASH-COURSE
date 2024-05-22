989a62e578edc4b8e32f882be55879bfad1a3ef1
LahiruPrasad12 <lahirupr471@gmail.com>
2024-05-22 11:59:47 +0530

    changes

--- 07_pointers/main.go
+++ 07_pointers/main.go
@@ -9,8 +9,8 @@ func main() {
 
 	myNumber := 25
 	var ptr2 = &myNumber
-	fmt.Println("Memory address of myNumber variable : ", ptr2)  //-> This will print the memory address of variable
-	fmt.Println("Memory address of myNumber variable : ", *ptr2) //-> This will print the value of variable
+	fmt.Println("Memory address of myNumber variable : ", ptr2) //-> This will print the memory address of variable
+	fmt.Println("Value of myNumber variable : ", *ptr2)         //-> This will print the value of variable
 
 	*ptr2 = *ptr2 + 2
 	fmt.Println("New value is : ", myNumber) //-> This will print the 27
