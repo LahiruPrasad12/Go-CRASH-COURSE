95c6508fd09588c7aefae214b259897ae7d0e98b
LahiruPrasad12 <lahirupr471@gmail.com>
2024-05-22 13:27:26 +0530

    changes

--- 16_methods/main.go
+++ 16_methods/main.go
@@ -21,14 +21,18 @@ type User struct {
 }
 
 //create a methods
-func (u User) getName() {
+func (u *User) getName() {
 	fmt.Println("Hi", u.Name)
 }
 
 //create function to change the user name
-func (u User) newName() {
+func (u *User) newName() {
 	u.Name = "bandara"
 	fmt.Println("New name is ", u.Name)
+}
+
+func test(name *int) {
+	*name = 10
 }
 
 /*
@@ -41,4 +45,35 @@ func (u User) newName() {
 		lahiru. meyt hetuvt tama mehi value ekk copy ekk pass kirima. itin menn mekt solution ekk vidiyt tama pointers enne. apita attam
 		real value ek pass krnn oni nm pointers tama use krnn oni. otherwise samahara velavat me una vge real value ek natuva
 		value eke copyk pass venva
+*/
+
+/*
+	uda eke aul kihipayak ava.
+
+	1. newName() eke pointer variabvle ekk haduvata *u.Name use kre na value ek assign krnn. ekt hetuva You don't need to use *u.Name = "bandara" because u.Name is syntactic sugar in Go
+	for (*u).Name.
+	attama newName() kiynne method ekk etkota method ekk akara dekai tiyenne. value reciver and ponter reciver.
+
+		1. value reciver method: 	func (u User) newName() {
+										u.Name = "bandara"
+										fmt.Println("New name is ", u.Name)
+									}
+
+		2. Pointer reciver method: 	func (u *User) newName() {
+										u.Name = "bandara"
+										fmt.Println("New name is ", u.Name)
+									}
+
+
+		meke value reciver ek compile and run una namut originam struct ek change krnne na. bcz metana u kiynne copy of the User struct. Modifying u.Name changes the copy,
+		not the original struct.
+
+		pointer reciever valadi u kiynne User struct eke copy ekk nevei. So ek Directly modifies the original User struct because it works with a pointer.
+
+		etkota pointer receiver ekkdi api *u.Name = "Bandara" dann oni na. bcz pointer receiver ekkdi Go la apita ek automatically krl denva.
+
+
+	Etkota meke test kiynne method ekk nevei function ekk. so eke recivers na. eke krl tiyenne pointer type argument ekk aran name kiyl. itin api ekt value ekk use krnvnm apit
+	*name = 10 vge denn venva
+
 */
