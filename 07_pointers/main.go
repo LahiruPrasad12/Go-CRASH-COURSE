b8307614a3ce0244ca715db5285a94a8e4a4bf51
LahiruPrasad12 <lahirupr471@gmail.com>
2024-05-22 13:28:19 +0530

    changes

--- 07_pointers/main.go
+++ 07_pointers/main.go
@@ -7,6 +7,8 @@ func main() {
 	var ptr1 *int
 	fmt.Println("Value of pointer is : ", ptr1) //-> this will print nil. bcz still doesnt assign any value
 
+	*ptr1 = 10
+
 	myNumber := 25
 	var ptr2 = &myNumber
 	fmt.Println("Memory address of myNumber variable : ", ptr2) //-> This will print the memory address of variable
@@ -14,5 +16,15 @@ func main() {
 
 	*ptr2 = *ptr2 + 2
 	fmt.Println("New value is : ", myNumber) //-> This will print the 27
+
+	/*
+		A pointer is a variable that holds the memory address of another variable. Instead of holding a value directly, it points to the location where the value is stored.
+
+		Why Use Pointers?
+			Efficiency: Passing large structs by pointer avoids copying the entire struct.
+			Modification: Allows functions to modify the original variable.
+
+
+	*/
 
 }
